f0f78dd21381759f08565287d34428fc94ad83e5
AlexKVal <alexkval@gmail.com>
2013-08-12 19:01:51 +0300

    turn needs refactoring

--- world.rb
+++ world.rb
@@ -1,6 +1,93 @@
 class World
-  def to_s
-    '.*' <<
-    '**.'
+
+  ALIVE = '*'
+  EMPTY = DEAD = '.'
+
+  attr_reader :max_row_number, :max_col_number
+
+  def initialize(text_world = "..\n..")
+    parse(text_world)
   end
+
+  def to_s
+    "World #{max_row_number+1}x#{max_col_number+1}"
+  end
+
+  def to_text
+    res = ''
+    @world_matrix.each do |line|
+      res += line * '' # array to string of concatenated chars ['.','*'] => ".*"
+      res += "\n"
+    end
+    res.chop # delete last \n (i.e. empty raw)
+  end
+
+  def alive?(row, col)
+    return false if row < 0 || col < 0 || col > @max_col_number || row > @max_row_number
+    @world_matrix[row][col] == ALIVE
+  end
+
+  def empty?(row, col)
+    !alive?(row, col)
+  end
+
+  # def empty?(row, col) # maybe refactor to return !alive?
+  #   return true if row < 0 || col < 0 || col > @max_col_number || row > @max_row_number
+  #   @world_matrix[row][col] == EMPTY
+  # end
+
+  def number_of_neighbours(row, col)
+    res = 0
+    res +=1 if alive?(row-1, col-1)
+    res +=1 if alive?(row-1, col)
+    res +=1 if alive?(row-1, col+1)
+    res +=1 if alive?(row,   col+1)
+    res +=1 if alive?(row+1, col+1)
+    res +=1 if alive?(row+1, col)
+    res +=1 if alive?(row+1, col-1)
+    res +=1 if alive?(row,   col-1)
+    res
+  end
+
+  def turn
+    new_world = []
+    row_num = 0
+    @world_matrix.each do |row|
+      new_world[row_num] = []
+
+      col_num = 0
+      row.each do |col|
+        new_world[row_num][col_num] = ( number_of_neighbours(row_num, col_num) < 2 ? DEAD : ALIVE )
+
+        col_num += 1
+      end
+
+      row_num += 1
+    end
+
+    @world_matrix = new_world
+    true #don't discover inside world
+  end
+
+private
+  def parse(text_world)
+    raise ArgumentError.new('Only . * \n allowed') if text_world =~ /[^\.\*\n]/
+    raise ArgumentError.new('Number of lines < 2') unless text_world.lines.count > 1
+
+    width = text_world.lines.first.delete("\n").size
+    raise ArgumentError.new('Number of columns < 2') if width < 2
+    @max_col_number = width - 1
+
+    @world_matrix = []
+    coord_y = 0
+
+    text_world.lines.each do |line|
+      line.delete!("\n")
+      raise ArgumentError.new('Not rectangle') if line.size != width # not rectangle
+      @world_matrix[coord_y] = line.split(//) # string to array of chars
+      coord_y += 1
+    end
+    @max_row_number = coord_y - 1
+  end
+
 end
--- world_test.rb
+++ world_test.rb
@@ -1,20 +1,200 @@
 require 'test/unit'
 require 'turn/autorun'
 
+Turn.config.format = :dot
+
 require './world'
 
 class WorldTest < Test::Unit::TestCase
   def setup
-    @wstate = World.new
+    @world = World.new("..\n..")
   end
 
-  def test_world_state_class_exist
-    assert_not_nil @wstate
+  def test_initializing_with_text_of_dots_and_asterisks
+    assert_nothing_raised( ArgumentError ) {World.new("..*\n**.")}
+    assert_raise( ArgumentError ) {World.new("..\n34")}
   end
 
-  def test_to_s_returns_text_of_dots_and_asterisks
-    assert_kind_of String, @wstate.to_s
-    assert_match /^[.*\n]*$/, @wstate.to_s
+  def test_to_text_returns_text_of_dots_and_asterisks
+    assert_kind_of String, @world.to_text
+    assert_match /^[.*\n]*$/, @world.to_text
   end
 
+  def test_minimal_world_is_2x2_dot_cells
+    assert_equal( World.new.to_text, "..\n.." )
+  end
+
+  def test_initial_text_world_has_to_be_rectangle
+    assert_raise( ArgumentError ) {World.new("**")}
+    assert_raise( ArgumentError ) {World.new("**\n*")}
+    assert_raise( ArgumentError ) {World.new("**\n*.\n***...***")}
+    assert_nothing_raised( ArgumentError ) {World.new("..***\n.....\n*****")}
+  end
+
+  def test_initial_text_world_cant_end_with_a_new_line
+    assert_nothing_raised( ArgumentError ) {World.new("..*\n...\n")}
+  end
+
+  def test_initial_text_world_has_to_be_at_least_2x2_or_bigger
+    assert_raise( ArgumentError ) {World.new(".")}
+    assert_raise( ArgumentError ) {World.new(".\n*")}
+    assert_nothing_raised( ArgumentError ) {World.new(".*\n*.")}
+  end
+
+  def test_what_we_put_that_we_get
+    assert_equal( World.new(".*\n*.").to_text, ".*\n*." )
+    assert_equal( World.new(".*.\n*.*").to_text, ".*.\n*.*" )
+    assert_equal( World.new(".*..\n*...\n....").to_text, ".*..\n*...\n...." )
+  end
+
+  def test_world_has_max_row_number
+    assert_respond_to @world, 'max_row_number'
+  end
+
+  def test_world_has_max_col_number
+    assert_respond_to @world, 'max_col_number'
+  end
+
+  def test_max_row_number_method_counts_right
+    assert_equal( 2, World.new(".*..\n*...\n....").max_row_number )
+    assert_equal( 1, World.new(".*..\n*...").max_row_number )
+    assert_equal( 1, World.new(".*..\n*...\n").max_row_number )
+  end
+
+  def test_max_col_number_method_counts_right
+    assert_equal( 3, World.new(".*..\n*...\n....").max_col_number )
+    assert_equal( 2, World.new(".*.\n*..").max_col_number )
+  end
+
+  def test_we_can_check_cell_is_alive
+    assert_respond_to @world, 'alive?'
+  end
+
+  def test_we_can_check_particular_cell_is_alive
+    example_world = World.new(".*.\n*.*")
+    # alive?(row, col) begins from 0,0
+    assert example_world.alive?(0,1), "but it isn't alive 0,1"
+    assert example_world.alive?(1,0), "but it isn't alive 1,0"
+    assert example_world.alive?(1,2), "but it isn't alive 1,2"
+    assert !example_world.alive?(0,0), "but it's alive 0,0"
+    assert !example_world.alive?(0,2), "but it's alive 0,2"
+    assert !example_world.alive?(1,1), "but it's alive 1,1"
+  end
+
+  def test_cell_above_the_world_matrix_is_not_alive
+    assert !World.new("**\n**").alive?(-1,0)
+  end
+
+  def test_cell_left_of_the_world_matrix_is_not_alive
+    assert !World.new("**\n**").alive?(0,-1)
+  end
+
+  def test_cell_right_of_the_world_matrix_is_not_alive
+    @ex_world = World.new("**\n**")
+    assert !@ex_world.alive?(0, @ex_world.max_col_number + 1)
+  end
+
+  def test_cell_under_the_world_matrix_is_not_alive
+    @ex_world = World.new("**\n**")
+    assert !@ex_world.alive?(@ex_world.max_row_number + 1, 0)
+  end
+
+  def test_we_can_check_cell_is_empty
+    assert_respond_to @world, 'empty?'
+  end
+
+  def test_we_can_check_particular_cell_is_empty
+    example_world = World.new(".*.\n*.*")
+    # empty?(row, col) begins from 0,0
+    assert example_world.empty?(0,0), "but it isn't empty 0,0"
+    assert example_world.empty?(0,2), "but it isn't empty 0,2"
+    assert example_world.empty?(1,1), "but it isn't empty 1,1"
+    assert !example_world.empty?(0,1), "but it's empty 0,1"
+    assert !example_world.empty?(1,0), "but it's empty 1,0"
+    assert !example_world.empty?(1,2), "but it's empty 1,2"
+  end
+
+  def test_we_can_check_number_of_neighbours_of_cell
+    assert_respond_to @world, 'number_of_neighbours'
+  end
+
+  def test_case_with_no_neighbours
+    txtwrld = <<EOS
+...
+...
+...
+EOS
+    example_world = World.new(txtwrld)
+    assert_equal 0, example_world.number_of_neighbours(1,1)
+  end
+
+  def test_case_with_8_neighbours
+    txtwrld = <<EOS
+***
+*.*
+***
+EOS
+    example_world = World.new(txtwrld)
+    assert_equal 8, example_world.number_of_neighbours(1,1)
+  end
+
+  def test_upper_left_edges_of_the_grid_is_empty
+    txtwrld = <<EOS
+*.*
+.*.
+EOS
+    example_world = World.new(txtwrld)
+    assert_equal 1, example_world.number_of_neighbours(0,0)
+  end
+
+  def test_bottom_right_edges_of_the_grid_is_empty
+    txtwrld = <<EOS
+*.*
+.*.
+EOS
+    example_world = World.new(txtwrld)
+    assert_equal 2, example_world.number_of_neighbours(1,2)
+  end
+
+  def test_just_check_complex_neighbours_count
+    txtwrld = <<EOS
+*.**.*
+.*.*..
+*..**.
+EOS
+    example_world = World.new(txtwrld)
+    assert_equal 5, example_world.number_of_neighbours(1,2)
+  end
+
+#======= Life's logic =========
+
+  def test_has_a_turn_method
+    assert_respond_to @world, 'turn'
+  end
+
+  def test_Any_live_cell_with_fewer_than_two_live_neighbours_dies__underpopulation
+    txtwrld = <<EOS
+*.*
+.*.
+*..
+EOS
+    ex_world = World.new(txtwrld)
+    assert ex_world.alive?(0,0), 'before turn 0,0'
+    assert ex_world.alive?(0,2), 'before turn 0,2'
+    assert ex_world.alive?(2,0), 'before turn 2,0'
+
+    ex_world.turn
+    assert !ex_world.alive?(0,0), 'after turn 0,0'
+    assert !ex_world.alive?(0,2), 'after turn 0,2'
+    assert !ex_world.alive?(2,0), 'after turn 2,0'
+  end
+
+#   def test_Any_live_cell_with_more_than_three_live_neighbours_dies__overcrowding
+#     txtwrld = <<EOS
+# *.*
+# .*.
+# *..
+# EOS
+#     ex_world = World.new(txtwrld)
+#   end
 end
